3db82c93527b0c70c213d7777d578ef5a72de406
Gbadamosi Emmanuel <blessedone093@gmail.com>
2019-10-20 02:26:37 -0700

    added package-lock.json file

---
+++ src/components/Product.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Product extends Component{
+render() {
+return(
+<div>
+<h3> Hello from Product</h3>
+</div>
+)
+
+}
+
+}
---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default  fuction  Title(name, title ){
+
+return(
+<div className="row">
+<div className="col-10 mx-auto my-2 text-center text-title">
+
+
+</div>
+
+</div>
+)
+
+
+}
